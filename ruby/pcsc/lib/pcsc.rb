44c72b5bdc534f0aa995545269577f69d917f0c5
a2800276 <a2800276@5fdf77b1-4124-0410-b011-2b42da5c28b3>
2008-09-24 09:16:12 +0000

    initial checkin

--- ruby/pcsc/lib/pcsc.rb
+++ ruby/pcsc/lib/pcsc.rb
@@ -1,32 +1,46 @@
-
+require File.dirname(__FILE__) + '/../ext/pcsc/PCSC_NATIVE'
 
 module PCSC
-  
-  
-
   class Context
     def initialize
-      @ctx = Lib.sCardEstablishContext 
+      @ctx = Driver.establishContext 
     end
     def release
-      Lib.sCardReleaseContext @ctx
+      Driver.releaseContext @ctx
     end
+
     # compare to Lib::Errors
     def is_valid
-      Lib.sCardIsValidContext @ctx
+      return true # TODO
     end
 
     def list_readers
-      Lib.sCardListReaders @ctx
+      tmp = Driver.listReaders @ctx
+      # returns "multistring, seperated by nul and ended by double null..."
+      tmp[0, tmp.size-2]
+      tmp.split("\000")
     end
+
 #    TODO
 #    def list_reader_groups
-#      Lib.sCardListReaderGroups @ctx
 #    end
-    def card_connect reader=nil, shared=Lib::ShareMode::SCARD_SHARE_SHARED, proto=Lib::Protocol::SCARD_PROTOCOL_ANY
+
+    def card_connect_internal reader=nil, shared=Lib::ShareMode::SCARD_SHARE_SHARED, proto=Lib::Protocol::SCARD_PROTOCOL_ANY
       reader ||= list_readers[0]
-      card, proto = Lib.sCardConnect @ctx, reader, shared, proto
-      Card.new(card, proto)
+      # TODO error handling no reader...
+      raise PCSCException.new("no readers available!") unless reader
+      #puts @ctx
+      #puts reader
+      #puts shared
+      #puts proto
+
+      card, proto = Driver.cardConnect @ctx, reader, shared, proto
+      return card, proto
+    end
+
+    def card_connect reader=nil, shared=Lib::ShareMode::SCARD_SHARE_SHARED, proto=Lib::Protocol::SCARD_PROTOCOL_ANY
+      card, proto = card_connect_internal(reader, shared, proto)
+      Card.new(card,proto)
     end
 
   end
@@ -37,10 +51,9 @@ module PCSC
     
     def self.connect
       ctx = Context.new
-      ctx.initialize
-      card, proto = ctx.card_connect
-      c = Card.new card, proto
-      c.ctx = ctx
+      card = ctx.card_connect
+      card.ctx = ctx
+      card
     end
 
     def initialize card, proto
@@ -48,36 +61,64 @@ module PCSC
       @proto=proto
     end
 
-    def reconnect shared=Lib::ShareMode::SCARD_SHARE_SHARED, proto=nil, init=Lib::Disposition::SCARD_LEAVE_CARD
-      proto ||= @proto
-      @proto = Lib.sCardReconnect @card, shared, proto, init 
-    end
+#    def reconnect shared=Lib::ShareMode::SCARD_SHARE_SHARED, proto=nil, init=Lib::Disposition::SCARD_LEAVE_CARD
+#      proto ||= @proto
+#      #@proto = Lib.sCardReconnect @card, shared, proto, init 
+#    end
 
     def disconnect disposition=Lib::Disposition::SCARD_UNPOWER_CARD
-      Lib.sCardDisconnect(@card, disposition)
-      @card = -1
-      @ctx && @ctx.release
+      Driver.cardDisconnect(@card, disposition)
+      @card = -1  # TODO necessary?
+      @ctx && @ctx.release # TODO, only do this if Card created Context itself, don't close other ppl's context...
     end
 
     def transaction disposition=Lib::Disposition::SCARD_LEAVE_CARD
-      Lib.sCardBeginTransaction @card
+      Driver.beginTx @card
       yield self
-      Lib.sCardEndTransaction @card, disposition
+      Driver.endTx @card, disposition
     end
 
-    def transmit
+    # bytes is the data to send, String for now, later should be APDU abstraction.
+    def transmit bytes, proto=nil, len=1024
+      proto ||= @proto # why use anything BUT the protocol we are connected with?
+      Driver.cardTransmit @card, bytes, proto, len  
     end
-
+  
+    # return [state, proto, atr]
     def status 
-      @state, @proto, @atr = Lib.sCardStatus(@card)
-      [@state, @proto, @atr]
+      @state, @proto, @atr_bytes = Driver.getStatus(@card)
+      [@state, @proto, @atr_bytes]
     end
-  end
+
+    def atr
+      @atr_bytes ||= status[2]
+      @atr ||= ISO7816::ATR.new @atr_bytes
+      @atr
+    end
+
+  end # Card 
 
   module Lib
-    require 'dl'
-    require 'dl/import'
+    module State
+      SCARD_UNKNOWN   =                0x0001	#/* Unknown state */
+      SCARD_ABSENT    =                0x0002	#/* Card is absent */
+      SCARD_PRESENT   =                0x0004	#/* Card is present */
+      SCARD_SWALLOWED =                0x0008	#/* Card not powered */
+      SCARD_POWERED   =                0x0010	#/* Card is powered */
+      SCARD_NEGOTIABLE=                0x0020	#/* Ready for PTS */
+      SCARD_SPECIFIC  =                0x0040	#/* PTS has been set */
 
+      STATES = {
+        SCARD_UNKNOWN    => "SCARD_UNKNOWN",    
+        SCARD_ABSENT     => "SCARD_ABSENT",     
+        SCARD_PRESENT    => "SCARD_PRESENT",    
+        SCARD_SWALLOWED  => "SCARD_SWALLOWED",  
+        SCARD_POWERED    => "SCARD_POWERED",    
+        SCARD_NEGOTIABLE => "SCARD_NEGOTIABLE", 
+        SCARD_SPECIFIC   => "SCARD_SPECIFIC",   
+      }
+
+    end
     module Scope
       # from pcsclite.h
       SCARD_SCOPE_USER = 0
@@ -189,139 +230,138 @@ module PCSC
       }
     end
     
-#    module SCards extend DL::Importable
-#      dlload '/System/Library/Frameworks/PCSC.framework/Versions/A/PCSC'
+##    module SCards extend DL::Importable
+##      dlload '/System/Library/Frameworks/PCSC.framework/Versions/A/PCSC'
+##    end
+#      
+#      
+#    LIB = DL.dlopen('/System/Library/Frameworks/PCSC.framework/Versions/A/PCSC')
+#
+#    def Lib.sCardEstablishContext
+#      @scec ||= LIB['SCardEstablishContext', 'LLPPP']
+#
+#      ptr = DL.malloc(DL.sizeof('L'))
+#      r,rs = @scec.call(Scope::SCARD_SCOPE_SYSTEM, nil, nil, ptr)
+#
+#      r &= 0xffffffff 
+#      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
+##      puts "%x" % ptr.to_i
+##      puts "%d" % ptr.to_i
+##      puts ptr.ptr.to_i
+##      obj = Object.new
+##      puts ptr.methods - obj.methods
+#      ret = ptr.ptr.to_i
+#      ptr.free
+#      return ret
 #    end
-      
-      
-    LIB = DL.dlopen('/System/Library/Frameworks/PCSC.framework/Versions/A/PCSC')
-
-    def Lib.sCardEstablishContext
-      @scec ||= LIB['SCardEstablishContext', 'LLPPP']
-
-      ptr = DL.malloc(DL.sizeof('L'))
-      r,rs = @scec.call(Scope::SCARD_SCOPE_SYSTEM, nil, nil, ptr)
-
-      r &= 0xffffffff 
-      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
-#      puts "%x" % ptr.to_i
-#      puts "%d" % ptr.to_i
-#      puts ptr.ptr.to_i
-#      obj = Object.new
-#      puts ptr.methods - obj.methods
-      ret = ptr.ptr.to_i
-      ptr.free
-      return ret
-    end
-
-    def Lib.sCardReleaseContext ctx
+#
+#    def Lib.sCardReleaseContext ctx
+##      puts ctx
+#      @scrc ||= LIB['SCardReleaseContext', 'LL']
+#      r,rs = @scrc.call(ctx)
+#      r &= 0xffffffff
+#      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
+#    end
+#
+#    def Lib.sCardIsValidContext ctx
 #      puts ctx
-      @scrc ||= LIB['SCardReleaseContext', 'LL']
-      r,rs = @scrc.call(ctx)
-      r &= 0xffffffff
-      puts "%x" % r
-      raise Errors::ERRS[r].to_s unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
-    end
-
-    def Lib.sCardIsValidContext ctx
-      puts ctx
-      @scivc ||= LIB['SCardIsValidContext', 'LL']
-      r, rs = @scivc.call(ctx)
-      r &= 0xffffffff
-      return 0
-    end
-
-    def Lib.sCardListReaders ctx
-      @sclr ||= LIB['SCardListReaders', 'LLPPP']
-      
-      longp = DL.malloc(DL.sizeof('L'))
-      r, rs = @sclr.call(ctx, nil, nil, longp)
-      len = longp.ptr.to_i
-      strp = DL.malloc(DL.sizeof('C')*len)
-      r, rs = @sclr.call(ctx, nil, strp, longp)
-      str =  strp.to_s(len-2) unless len <=2
-      str && str.split("\0") 
-    end
-
-    def Lib.sCardConnect ctx, reader, shared, proto
-      @sccc ||= LIB['SCardConnect', 'LLSLLPP']
-      cardp = DL.malloc(DL.sizeof('L'))
-      protop = DL.malloc(DL.sizeof('L'))
-
-      r,rs = @sccc.call(ctx, reader, shared, proto, cardp, protop)
-      r &= 0xffffffff 
-      #puts "%x" % r
-      
-      card = cardp.ptr.to_i
-      proto = protop.ptr.to_i
-  puts card
-  puts proto
-      #raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
-      return card, proto
-    end
-
-  def Lib.sCardReconnect card, shared, proto, init 
-      @scrc ||= LIB['SCardReconnect', 'LLLLLP']
-      protop = DL.malloc(DL.sizeof('L'))
-
-      r,rs = @scrc.call(card, shared, proto, init, protop)
-      r &= 0xffffffff 
-#      puts "%x" % r
-      
-      proto = protop.ptr.to_i
+#      @scivc ||= LIB['SCardIsValidContext', 'LL']
+#      r, rs = @scivc.call(ctx)
+#      r &= 0xffffffff
+#      return 0
+#    end
+#
+#    def Lib.sCardListReaders ctx
+#      @sclr ||= LIB['SCardListReaders', 'LLPPP']
+#      
+#      longp = DL.malloc(DL.sizeof('L'))
+#      r, rs = @sclr.call(ctx, nil, nil, longp)
+#      len = longp.ptr.to_i
+#      strp = DL.malloc(DL.sizeof('C')*len)
+#      r, rs = @sclr.call(ctx, nil, strp, longp)
+#      str =  strp.to_s(len-2) unless len <=2
+#      str && str.split("\0") 
+#    end
+#
+#    def Lib.sCardConnect ctx, reader, shared, proto
+#      @sccc ||= LIB['SCardConnect', 'LLSLLPP']
+#      cardp = DL.malloc(DL.sizeof('L'))
+#      protop = DL.malloc(DL.sizeof('L'))
+#
+#      r,rs = @sccc.call(ctx, reader, shared, proto, cardp, protop)
+#      r &= 0xffffffff 
+#      #puts "%x" % r
+#      
+#      card = cardp.ptr.to_i
+#      proto = protop.ptr.to_i
 #  puts card
 #  puts proto
-      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
-      return proto
-    end
-
-    def Lib.sCardDisconnect card, disposition
-      puts "Disconnect for #{card}"
-      @scdc ||= LIB['SCardDisconnect', 'LLL']
-      r, rs = @scdc.call(card, disposition)
-      r &= 0xffffffff
-      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
-
-    end
-
-    def Lib.sCardBeginTransaction card
-      @scbt ||= LIB['SCardBeginTransaction','LL']
-      r, rs = @scbt.call(card)
-      r &=0xffffffff
-      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
-    end
-  
-    def Lib.sCardEndTransaction card, disposition
-      @scet ||= LIB['SCardEndTransaction', 'LLL']
-      r, rs = @scet.call(card, disposition)
-    end
-
-    def Lib.sCardStatus card
-      puts "sCardStatus for #{card}"
-      @scst ||= LIB['SCardStatus','Llslllsl']
-
-      # card, readerName, readerNameLen
-      stateP = DL.malloc(DL.sizeof('L'))
-      protoP = DL.malloc(DL.sizeof('L'))
-      atrLenP = DL.malloc(DL.sizeof('L'))
-      nameLen = DL.malloc(DL.sizeof('L'))
-      #MAX_ATR_SIZE=33
-      atrP = DL.malloc(DL.sizeof('C')*330)
-      nameP = DL.malloc(DL.sizeof('C')*330)
-      r,rs = @scst.call(card, nameP, 330, 0, 0, atrP, 330)
-      r&=0xffffffff
-      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
-
-      
-      state=stateP.ptr.to_i
-      proto=protoP.ptr.to_i
-      atr=atrP.to_s(atrLenP.ptr.to_i)
-puts "state #{state}, #{stateP.to_i}"
-puts proto
-puts atrLenP.ptr.to_i
-      [state, proto, atr]
-    end
-
+#      #raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
+#      return card, proto
+#    end
+#
+#  def Lib.sCardReconnect card, shared, proto, init 
+#      @scrc ||= LIB['SCardReconnect', 'LLLLLP']
+#      protop = DL.malloc(DL.sizeof('L'))
+#
+#      r,rs = @scrc.call(card, shared, proto, init, protop)
+#      r &= 0xffffffff 
+##      puts "%x" % r
+#      
+#      proto = protop.ptr.to_i
+##  puts card
+##  puts proto
+#      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
+#      return proto
+#    end
+#
+#    def Lib.sCardDisconnect card, disposition
+#      puts "Disconnect for #{card}"
+#      @scdc ||= LIB['SCardDisconnect', 'LLL']
+#      r, rs = @scdc.call(card, disposition)
+#      r &= 0xffffffff
+#      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
+#
+#    end
+#
+#    def Lib.sCardBeginTransaction card
+#      @scbt ||= LIB['SCardBeginTransaction','LL']
+#      r, rs = @scbt.call(card)
+#      r &=0xffffffff
+#      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
+#    end
+#  
+#    def Lib.sCardEndTransaction card, disposition
+#      @scet ||= LIB['SCardEndTransaction', 'LLL']
+#      r, rs = @scet.call(card, disposition)
+#    end
+#
+#    def Lib.sCardStatus card
+#      puts "sCardStatus for #{card}"
+#      @scst ||= LIB['SCardStatus','Llslllsl']
+#
+#      # card, readerName, readerNameLen
+#      stateP = DL.malloc(DL.sizeof('L'))
+#      protoP = DL.malloc(DL.sizeof('L'))
+#      atrLenP = DL.malloc(DL.sizeof('L'))
+#      nameLen = DL.malloc(DL.sizeof('L'))
+#      #MAX_ATR_SIZE=33
+#      atrP = DL.malloc(DL.sizeof('C')*330)
+#      nameP = DL.malloc(DL.sizeof('C')*330)
+#      r,rs = @scst.call(card, nameP, 330, 0, 0, atrP, 330)
+#      r&=0xffffffff
+#      raise Errors::ERRS[r] unless r == Errors::SCARD_S_SUCCESS # TODO error mappings
+#
+#      
+#      state=stateP.ptr.to_i
+#      proto=protoP.ptr.to_i
+#      atr=atrP.to_s(atrLenP.ptr.to_i)
+#puts "state #{state}, #{stateP.to_i}"
+#puts proto
+#puts atrLenP.ptr.to_i
+#      [state, proto, atr]
+#    end
+#
 
   end #module lib 
 
@@ -330,9 +370,9 @@ end # module PCSC
 
 if $0 == __FILE__
   ctx = PCSC::Context.new
-  #puts ctx.is_valid == 0
-  puts ctx.list_readers
   ctx.release
+  puts ctx.is_valid == 0
+  puts ctx.list_readers
   card = ctx.card_connect 
   puts card
   puts card.status[2].size
